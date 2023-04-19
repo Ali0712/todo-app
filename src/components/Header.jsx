aaedc5f3a0770fec02759879c3a38ee37816f871
Muhammad Ali <muhammadali07122@gmail.com>
2023-04-20 02:30:42 +0500

    finalize

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import logo from "./logo2.png";
 
 function Header() {
 	return (
 		<>
 			<nav className="navbar">
 
-				<h1 className="navbar-heading"><img className='logo' src="../src/logo.png" alt="logo" />PlanMate</h1>
+				<h1 className="navbar-heading"><img className='logo' src={logo} alt="logo" />PlanMate</h1>
 				<p className="navbar-tagline">Your Perfect Planning Companion</p>
 			</nav>
 		</>
